92ddd6034a8ed83ae01e24250b2b118ad8e88d93
Bharath <survepalli.bharat@gmail.com>
2016-01-05 05:30:40 +0530

    Splash Screen Integrated.

--- Squeegy/app/src/main/java/com/squeegy/android/ui/activity/SplashActivity.java
+++ Squeegy/app/src/main/java/com/squeegy/android/ui/activity/SplashActivity.java
@@ -1,23 +1,35 @@
 package com.squeegy.android.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.squeegy.android.R;
 import com.squeegy.android.base.BaseActivity;
+import com.squeegy.android.util.AppConstants;
 
 
-public class SplashActivity extends BaseActivity{
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        hideActionBar();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        }, AppConstants.SPLASH_DELAY);
+
     }
 
-    private void hideActionBar() {
-
-        if(getSupportActionBar()!=null) {
-            getSupportActionBar().hide();
-        }
-    }
+//    private void hideActionBar() {
+//
+//        if(getSupportActionBar()!=null) {
+//            getSupportActionBar().hide();
+//        }
+//    }
 }
---
+++ Squeegy/app/src/main/java/com/squeegy/android/util/AppConstants.java
@@ -0,0 +1,7 @@
+package com.squeegy.android.util;
+
+
+public class AppConstants {
+
+    public static final int SPLASH_DELAY = 1500;
+}
